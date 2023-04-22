8c48c5f42c4c37077053ae0aa0e3b55abfeb4453
ASNstoleMYname <12aslan.umarov@gmail.com>
2023-04-22 13:13:25 +0200

    added search and add command. first useable version of programm. => add delete contact func

--- contact-book.py
+++ contact-book.py
@@ -11,11 +11,11 @@
 
 import sys
 import sqlite3 
-import nerdfonts as nf
+# import nerdfonts as nf
 
 
 def ascii_start():
-    print("                       |--|            |--|")
+    print("                        __              __")
     print(" .----.-----.-----.----|  |_.---.-.----|  |_.-----.")
     print(" |  __|  _  |     |  __|   _|  _  |  __|   _|__ --|")
     print(" |____|_____|__|__|____|____|___._|____|____|_____|")
@@ -32,41 +32,89 @@ cur = connected.cursor() # initizliazing cursor, needed to fetch date form .db
 
 
 # funcitons
-def print_all():
+
+def help():
+    print(">=============================================<")
+    print(" *   see all => see all entries")
+    print(" *   search => search buy name(SQL key)")
+    print(" *   add => add new entrie")
+
+
+def add_contact():
+    contact_type = input("(P)erson or (C)ompanie:")
+
+    match contact_type:
+        case "p":
+            contact_naam = input("name:")
+            contact_a_naam = input("last name:")
+            contact_nummer = input("nummer:")
+            contact_email = input("email:")
+            cur.execute("""INSERT INTO people (naam,a_naam,nummer,email) VALUES (?,?,?,?)""", [contact_naam,contact_a_naam,contact_nummer,contact_email]) 
+        case "c":
+            contact_naam = input("name:")
+            contact_nummer = input("nummer:")
+            contact_email = input("email:")
+            contact_addres = input("straat:")
+            cur.execute("""INSERT INTO companies (naam,nummer,email,addres) VALUES (?,?,?,?)""", [contact_naam,contact_email,contact_nummer,contact_addres])
+
+
+def print_all(): #prints all contacts
+    #----people 
     cur.execute("SELECT * FROM people")
-    rows = cur.fetchall()
+    rows = cur.fetchall() #puts cursor elements in a tuplet
     print(">-|people|--------------------------------------------------------<")
     for x in rows:
         # print(nf.icons['nf-cod-archive'])
-        print(">-" +" | ".join(x))
+        print(">-" +" | ".join(x)) #print the tuplet in a nicer way
         # print(">----------------------------------------------------------<")
+    
+    #----companies
     cur.execute("SELECT * FROM companies")
     rows = cur.fetchall()
     print(">-|companies|-----------------------------------------------------<")
     for x in rows:
         print(">-" + " | ".join(x))
         # print(">----------------------------------------------------------<")
+
+
+def search_all():
+    table_select=input("witch table? (P)eople (C)ompanies: ")
+    name_search = input("name?:")
+    match table_select:
+        case "p":
+            cur.execute("""Select * From people Where naam like ?""", [name_search] ) 
+
+        case "c":
+            cur.execute("""SELECT * FROM companies WHERE naam LIKE ?""", [name_search])
+
+
 def close_connection():
-    # clossing connection
+    # closing connection
     if (connected):
-        connected.close()
+        connected.close() 
         print("=== \\\===============|closed|===============// ===")
         sys.exit()
         
 
 
 # try for error catching
-
 while True:
     try:
         usr_input = input("||>")
         match usr_input:
-            case "close":
+            case "close": #close program
                 close_connection()
-            case "see all":
+            case "see all": # see all entries
                 print_all ()
-
-
+            case "search": # search for specifiek entrie
+                search_all()
+                result = cur.fetchall()
+                print(result)
+            case "add": # add an entrie
+                add_contact()
+                connected.commit()
+            case "help":
+                help()
     # error catching
     except sqlite3.Error as e:
         print("Error!:", e)
